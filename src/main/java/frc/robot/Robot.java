2321cd3f3734f545f19874dfa0e955a792018b30
AtomicVikings5256 <AtomicVikings5256@gmail.com>
2020-01-25 09:59:46 -0500

    Revert "Navx?"
    This reverts commit fc3849d0319ef75c322e4e2118de6a1b19619d3c.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,27 +1,30 @@
 //Neccesary Packages
 package frc.robot;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
-
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 //Smart Dashboard
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.CAN;
+
+import com.kauailabs.navx.frc.AHRS;
 
 //Color Stuffs
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.util.Color;
 import com.revrobotics.ColorSensorV3;
-
 //Motor Controllers
-import edu.wpi.first.wpilibj.CAN;
 import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.Talon;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import com.kauailabs.navx.frc.AHRS;
-
 //Drive
-import edu.wpi.first.wpilibj.drive.DifferentialDrive; 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Timer;
 
 //NetworkTables
 import edu.wpi.first.networktables.NetworkTable;
@@ -30,8 +33,13 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 
 
 public class Robot extends TimedRobot {
-  //NAVX
-  private AHRS ahrs;
+  AHRS ahrs;
+
+  //Things?
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String autoSelected;
+  private final SendableChooser<String> chooser = new SendableChooser<>();
 
   //NetworkTable
   NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
@@ -45,77 +53,149 @@ public class Robot extends TimedRobot {
   
   //Drive 
   private DifferentialDrive drive;
-  private WPI_TalonFX           rightFront, leftFront, rightRear, leftRear;
-  private SpeedControllerGroup leftSide, rightSide;
+  private WPI_TalonFX           topRightDrive, topLeftDrive, bottomRightDrive, bottomLeftDrive;
 
   //Mechanisms
-  private CANSparkMax intakeyBoi, rolleyBoi, turretBoi, leftClimbyBoi, rightClimbyBoi;
-  private VictorSPX   conveyorBoi;
-  private WPI_TalonFX     leftShootyBoi, rightShootyBoi;
-  
+  private CANSparkMax intakey, rolley, turret, leftClimby, rightClimby;
+  private VictorSPX   conveyor;
+  private WPI_TalonFX     leftShooty, rightShooty;
+
+  //Controllers
+  private Joystick logitechAlpha, logitechBeta;
+
+  //Limelight stuffs
+  private boolean LimelightHasTarget = false;
+  private double LimelightDriveCommand = 0.0;
+  private double LimelightSteerCommand = 0.0;
+
+  public void operatorControl() {
+    while (isOperatorControl() && isEnabled()) {
+      Timer.delay(0.020); /* wait for one motor time period (50Hz) */
+
+      boolean zero_yaw_pressed = logitechAlpha.getTrigger();
+      if (zero_yaw_pressed) {
+        ahrs.zeroYaw();
+      }
+
+      /* Display Processed Acceleration Data (Linear Acceleration, Motion Detect) */
+      SmartDashboard.putNumber( "IMU_Accel_X",    ahrs.getWorldLinearAccelX());
+      SmartDashboard.putNumber( "IMU_Accel_Y",    ahrs.getWorldLinearAccelY());
+      SmartDashboard.putBoolean("IMU_IsMoving",   ahrs.isMoving());
+      SmartDashboard.putBoolean("IMU_IsRotating", ahrs.isRotating());
+
+      SmartDashboard.putNumber( "RawAccel_X",     ahrs.getRawAccelX());
+      SmartDashboard.putNumber( "RawAccel_Y",     ahrs.getRawAccelY());
+      SmartDashboard.putNumber( "RawAccel_Z",     ahrs.getRawAccelZ());
+      
+    }
+  }
+
   @Override
   public void robotInit() {
     //Mech
-    leftShootyBoi = new WPI_TalonFX(5);
-    rightShootyBoi = new WPI_TalonFX(6);
+    leftShooty = new WPI_TalonFX(5);
+    rightShooty = new WPI_TalonFX(6);
 
-    //NAVX
-    try {
-      ahrs = new AHRS(I2C.Port.kMXP);
-    } catch (RuntimeException ex) {
-      SmartDashboard.putString("Error", "There was an error initing the navX: " + ex.getMessage());
-    }
-    
-    //Drive
-    rightFront = new WPI_TalonFX(7);
-    leftFront = new WPI_TalonFX(8);
-    rightRear = new WPI_TalonFX(9);
-    leftRear = new WPI_TalonFX(10);
-    leftSide = new SpeedControllerGroup(leftFront, leftRear);
-    rightSide = new SpeedControllerGroup(rightFront, rightRear);
+    //SmartDashboard (prob dont need)
+    chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", chooser);
 
-    DifferentialDrive drive = new DifferentialDrive(leftSide, rightSide);
   }
 
   @Override
   public void robotPeriodic() {
-    //Color Sensor
-      final Color detectedColor = colorSensor.getColor();
-      final double IR = colorSensor.getIR();
+    final Color detectedColor = colorSensor.getColor();
+    final double IR = colorSensor.getIR();
 
-      SmartDashboard.putNumber("Red", detectedColor.red);
-      SmartDashboard.putNumber("Green", detectedColor.green);
-      SmartDashboard.putNumber("Blue", detectedColor.blue);
-      SmartDashboard.putNumber("IR", IR);
+    SmartDashboard.putNumber("Red", detectedColor.red);
+    SmartDashboard.putNumber("Green", detectedColor.green);
+    SmartDashboard.putNumber("Blue", detectedColor.blue);
+    SmartDashboard.putNumber("IR", IR);
 
-      final int proximity = colorSensor.getProximity();
+    final int proximity = colorSensor.getProximity();
 
-      SmartDashboard.putNumber("Proximity", proximity);
-    // Limelight
-      
+    SmartDashboard.putNumber("Proximity", proximity);
+
+    //read values periodically
+    final double x = tx.getDouble(0.0);
+    final double y = ty.getDouble(0.0);
+    final double area = ta.getDouble(0.0);
+    // post to smart dashboard periodically
+    SmartDashboard.putNumber("LimelightX", x);
+    SmartDashboard.putNumber("LimelightY", y);
+    SmartDashboard.putNumber("LimelightArea", area);
+
   }
 
   @Override
   public void autonomousInit() {
-
+    autoSelected = chooser.getSelected();
   }
 
   @Override
   public void autonomousPeriodic() {
-    
+
   }
 
   @Override
   public void teleopPeriodic() {
+    Update_Limelight_Tracking();
+
+    double varSteer = logitechBeta.getX(Hand.kRight);
+    double varDrive = -logitechBeta.getY(Hand.kLeft);
+    final boolean auto = logitechBeta.getRawButton(1);
+
+    varSteer *= 0.70;
+    varDrive *= 0.70;
+
+    if (auto) {
+      if (LimelightHasTarget) {
+        drive.arcadeDrive(LimelightDriveCommand, LimelightSteerCommand);
+      } else {
+        drive.arcadeDrive(0.0, 0.0);
+      }
+    } else {
+      drive.arcadeDrive(varDrive, varSteer);
+    }
 
   }
 
   @Override
   public void testPeriodic() {
-
   }
 
-  void OperatorControl() {
-    float left_command = 
+  public void Update_Limelight_Tracking() {
+    final double STEER_K = 0.03;
+    final double DRIVE_K = 0.26;
+    final double DESIRED_TARGET_AREA = 13.0;
+    final double MAX_DRIVE = 0.7;
+
+    final double tv = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
+    final double tx = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
+    final double ty = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0);
+    final double ta = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0);
+
+    if (tv < 1.0) {
+      LimelightHasTarget = false;
+      LimelightDriveCommand = 0.0;
+      LimelightSteerCommand = 0.0;
+    }
+
+    LimelightHasTarget = true;
+
+    // start with proportional steering
+    double steer_cmd = tx * STEER_K;
+    LimelightSteerCommand = steer_cmd;
+
+    // try to drive forward until the target area reaches our desired area
+    double drive_cmd = (DESIRED_TARGET_AREA - ta) * DRIVE_K;
+
+    // don't let the robot drive too fast into the goal
+    if (drive_cmd > MAX_DRIVE) {
+      drive_cmd = MAX_DRIVE;
+    }
+    LimelightDriveCommand = drive_cmd;
+
   }
 }
